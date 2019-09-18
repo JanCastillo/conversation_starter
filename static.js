4f58a9fcc21f1b4227bbfe15333ae321dfdd71db
Jpkemperor <jan_jacm123@hotmail.com>
2019-09-18 14:19:17 -0500

    adding agree/disagree

--- static.js
+++ static.js
@@ -1,13 +1,17 @@
 var items1 = data.questions;
 var items2 = data.phrasal_verbs;
+var items3 = data.agree;
+var items4 = data.disagree;
 
 var button1 = d3.select("#questions");
 var button2 = d3.select("#verbs");
+var button3 = d3.select("#agree");
+var button4 = d3.select("#disagree");
 
 button1.on("click", function() {
     d3.event.preventDefault();
     let i;
-    d3.select("ul").append("br");
+    d3.select("#list1").append("br");
     for (i = 0; i < 5; i++) { 
         let random = Math.floor((Math.random() * items1.length));
         let randomItem = items1[random];
@@ -21,7 +25,7 @@ button1.on("click", function() {
 button2.on("click", function() {
     d3.event.preventDefault();
     let i;
-    let verbs = d3.select("ul")
+    let verbs = d3.select("#list1")
     let table = verbs.append("table")
     verbs.append("br")
     let tbody = table.append("tbody")
@@ -32,4 +36,30 @@ button2.on("click", function() {
         let cell = row.append("td")
         cell.text(randomItem)
     }
+  });
+
+  button3.on("click", function() {
+    d3.event.preventDefault();
+    let expression = d3.select("#list2")
+    let table = expression.append("table")
+    expression.append("br")
+    let tbody = table.append("tbody")
+        let random = Math.floor((Math.random() * items3.length));
+        let randomItem = items3[random];
+        let row = tbody.append("tr")
+        let cell = row.append("td")
+        cell.text(randomItem)
+  });
+
+  button4.on("click", function() {
+    d3.event.preventDefault();
+    let expression = d3.select("#list2")
+    let table = expression.append("table")
+    expression.append("br")
+    let tbody = table.append("tbody")
+        let random = Math.floor((Math.random() * items4.length));
+        let randomItem = items4[random];
+        let row = tbody.append("tr")
+        let cell = row.append("td")
+        cell.text(randomItem)
   });
